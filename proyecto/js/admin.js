c6e0124689fb36dbef82e8883374210e3878512e
Bruno Tripi <brunoweh@hotmail.com>
2023-11-25 12:16:14 -0300

    mejoras admin

--- proyecto/js/admin.js
+++ proyecto/js/admin.js
@@ -1,17 +1,26 @@
 const tablaUsuario = document.querySelector("#tablaUsuario");
-const formularioProducto = document.querySelector("#formularioProducto");
-const tablaProducto = document.querySelector("#tablaProducto");
+const formularioJuegos = document.querySelector("#formularioJuegos");
+const tablaJuego = document.querySelector("#tablaJuego");
+
+let usuariosPrueba = [
+  {
+    id: 123,
+    nombre: "messi",
+    email: "as@hotmail.com",
+  },
+];
+localStorage.setItem("usuarios", JSON.stringify(usuariosPrueba));
 
 let usuarios = JSON.parse(localStorage.getItem("usuarios"));
 
-let productos = [];
-
 //evento
-formularioProducto.addEventListener("submit", crearProducto);
+formularioJuegos.addEventListener("submit", crearJuego);
 
-class producto {
-  constructor(Codigo, nombre, categoria, descripcion) {
-    this.Codigo = Codigo;
+let juegos = JSON.parse(localStorage.getItem("juegos"));
+
+class juego {
+  constructor(codigo, nombre, categoria, descripcion) {
+    this.codigo = codigo;
     this.nombre = nombre;
     this.categoria = categoria;
     this.descripcion = descripcion;
@@ -20,31 +29,39 @@ class producto {
 
 //mostrar los usuarios en la tabla
 function cargarTablaUsuario() {
-  usuarios.map(function (usuario) {
-    let tr = document.createElement("tr");
+  if (usuarios.length > 0) {
+    usuarios.map(function (usuario) {
+      let tr = document.createElement("tr");
 
-    tr.innerHTML = `
-                <td class="row">${usuario.id}</td>
-                <td>${usuario.nombre}</td>
-                <td>${usuario.email}</td>`;
+      tr.innerHTML = `
+                <td class='row align-middle'>${usuario.id}</td>
+                <td class='align-middle d-none d-md-table-cell'>${usuario.nombre}</td>
+                <td class='align-middle '>${usuario.email}</td>
+                <td><button class="btn btn-info" id="botonEdit"><i class="fa-regular fa-pen-to-square"></i></button>  
+                <button class="btn btn-danger" id="botonEdit2"><i class="fa-solid fa-delete-left"></i></button></td>`;
 
-    tablaUsuario.appendChild(tr);
-  });
+      tablaUsuario.appendChild(tr);
+    });
+  } else {
+    error("no existen usuarios");
+  }
 }
 
 cargarTablaUsuario();
 
-function crearProducto(e) {
+// crear juegos
+function crearJuego(e) {
   e.preventDefault();
 
-  const id = Date.now();
+  const codigo = `${Date.now()}`;
   const nombre = document.querySelector("#nombre").value;
   const categoria = document.querySelector("#categoria").value;
   const descripcion = document.querySelector("#descripcion").value;
 
   //validacioness
-  if (nombre == "" || precio == "" || descripcion == "") {
+  if (nombre === "" || categoria === "" || descripcion === "") {
     error("Los campos no pueden estar vacios");
+    return;
   } else if (descripcion.length > 600) {
     error("la descripcion debe tener menor de 600 caracteres ");
   } else {
@@ -53,34 +70,42 @@ function crearProducto(e) {
       icon: "success",
       title: "Producto Cargado Correctamente",
       showConfirmButton: false,
-      timer: 2500,
+      timer: 2000,
     });
   }
 
-  const newproduct = new producto(id, nombre, precio, descripcion);
+  const newgames = new juego(codigo, nombre, categoria, descripcion);
 
-  productos.push(newproduct);
+  juegos.push(newgames);
 
-  localStorage.setItem("productos", JSON.stringify(productos));
+  localStorage.setItem("juegos", JSON.stringify(juegos));
 
-  formularioProducto.reset();
+  formularioJuegos.reset();
+
+  cargarTablaJuego();
 }
 
-function cargarTablaProducto() {
-  productos.map(function (producto) {
+function cargarTablaJuego() {
+  tablaJuego.innerHTML = "";
+
+  juegos.map(function (jg) {
     let tr = document.createElement("tr");
 
     tr.innerHTML = `
-                <td class="row">${producto.id}</td>
-                <td>${producto.nombre}</td>
-                <td>${producto.categoria}</td>
-                <td>${producto.descripcion}</td>`;
+                <td class='row align-middle '>${jg.codigo}</td>
+                <td class='align-middle'>${jg.nombre}</td>
+                <td class='align-middle'>${jg.categoria}</td>
+                <td class='align-middle d-none d-md-table-cell'>${jg.descripcion}</td>
+                <td>
+                <button class="btn btn-info" id="botonEdit"><i class="fa-regular fa-pen-to-square"></i></button>  
+                <button class="btn btn-danger" id="botonEdit2" onclick='borrarJuegos (${jg.codigo})'><i class="fa-solid fa-delete-left"></i></button> 
+                </td>`;
 
-    tablaProducto.appendChild(tr);
+    tablaJuego.appendChild(tr);
   });
 }
 
-cargarTablaProducto();
+cargarTablaJuego();
 
 function error(mensaje) {
   parrafo.textContent = mensaje;
@@ -88,4 +113,13 @@ function error(mensaje) {
   setTimeout(() => {
     parrafo.textContent = "";
   }, 6000);
+}
+
+// funcion borrar juegos
+
+function borrarJuegos(codigo) {
+  juegos = juegos.filter(function (jg) {
+    return jg.codigo !== codigo;
+  });
+  console.log(juegos);
 }
